66a0986a4207351bc17d1e83f292222f2d5533e4
Alex Crichton <alex@alexcrichton.com>
2019-03-07 07:29:06 -0800

    Remove the key when we're done with it

--- ci/setup-git.sh
+++ ci/setup-git.sh
@@ -5,5 +5,6 @@ export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no"
 echo $GITHUB_DEPLOY_KEY | base64 -d > _the_key
 chmod 600 _the_key
 ssh-add _the_key
+rm _the_key
 git config --global user.name "Deploy from CI"
 git config --global user.email ""
